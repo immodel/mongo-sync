506c053801eeb064c328386c78b04d57e47ffd30
Andrew Shaffer <darawk@gmail.com>
2015-03-09 01:23:34 -0700

    immutable data

--- index.js
+++ index.js
@@ -4,55 +4,64 @@ var objectid = require('objectid');
 
 module.exports = function(mongoUrl) {
   var db = monk(mongoUrl);
-  
+
   return function() {
-    require('immodel-mongo-id')(this);
-    
+    this.use(require('immodel-mongo-id'));
+    this.attr('_id', 'ObjectID');
+
     this.collection = function(name) {
       return this.use(function() {
         this.collectionName = name;
       });
     };
-    
+
     this.db = function() {
       return db.get(this.collectionName);
     };
-    
+
     this.query = function() {
       return mquery(this.db());
     };
-       
-       
-    this.attr('_id', 'ObjectID');
+
+    this.prototype.query = function() {
+      return this.model.query();
+    };
+
+    this.prototype.save = function(cb) {
+      var doc = this;
+
+      doc.run('saving', function(err, doc) {
+        if(err) return cb(err);
+
+        var json = doc.toJSON();
+        var model = doc.model;
+
+        function done(err, json) {
+          if(err) return cb(err);
+          var doc = new model(json);
+          doc.run('saved', cb);
+        }
+
+        if(doc.isNew) {
+          model.db().insert(json, done);
+        } else {
+          doc.query()
+            .where('_id', json._id)
+            .update(json, function(err, numAffected) {
+              if(! err && numAffected === 0) return err = new Error('Document not found');
+              done(err, json);
+            });
+        }
+      });
+    };
+
     this.on('init', function(evt) {
       var doc = evt.doc;
       doc.isNew = ! doc.value._id;
-      if(doc.isNew)
-        doc._id = objectid();
-      
-      doc.query = function() {
-        return this.model.query();
-      };
-      
-      doc.save = function(cb) {
-        var json = this.toJSON();
-        var model = this.model;
-        
-        if(this.isNew) {
-          this.model.db().insert(json, function(err, doc) {
-            if(err) return cb(err);
-            cb(null, new model(doc));
-          });
-        } else {
-          this.query()
-            .where('_id', json._id)
-            .update(json, function(err, numAffected) {
-              if(err) return cb(err);
-              if(numAffected === 0) return cb(new Error('Document not found'));
-              cb(null, new model(json));
-            });
-        }
-      };
-    })
+      var err = new Error;
+      if(doc.isNew) {
+        doc.value._id = objectid().toString();
+      }
+    });
   };
 };
--- test/index.js
+++ test/index.js
@@ -4,20 +4,30 @@ var model = require('immodel')
 
 var Collection = model
   .use(require('../')('localhost/test'))
-  
+
 describe('mongo-sync', function() {
   it('should work', function(done) {
     var User = Collection
       .collection('users')
       .attr('username', 'string');
-  
+
     var doc = new User({username: 'test'});
 
     doc.save(function(err, user) {
       assert(err === null);
-      assert(user.get('username') === 'test');
-      assert(user.get('_id') === doc.get('_id'));
+      assert(user.get('username').value === 'test');
+      assert(user.get('_id').value, doc.get('_id').value);
+      assert(user.get('_id').value === doc.get('_id').value);
       done();
     });
+  });
+
+  it('should populate object ids on new documents', function() {
+    var User = Collection
+      .collection('users')
+      .attr('username', 'string');
+
+    var user = new User();
+    assert(user.get('_id').value);
   });
 });
